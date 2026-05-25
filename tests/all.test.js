fb384e8d11ab349355fb3e7abc8625e6559af29a
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2026-05-25 22:10:52 +0900

    feat: add sectionConfigurations option

--- tests/all.test.js
+++ tests/all.test.js
@@ -2,4 +2,5 @@ import "./declaration-order/core.test.js";
 import "./declaration-order/options.test.js";
 import "./declaration-order/pinning.test.js";
 import "./declaration-order/regressions.test.js";
+import "./declaration-order/section-configurations.test.js";
 import "./declaration-order/typescript.test.js";
